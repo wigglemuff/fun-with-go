50e56728ec9e50338d9e088ba72eb38c71ecd7d9
Omkar Bhagat <omkarbhagat93+github@gmail.com>
2024-11-07 23:23:42 +0000

    basic leetcode tree problems 2

--- leetcode-basic-tree-problems/main.go
+++ leetcode-basic-tree-problems/main.go
@@ -107,6 +107,98 @@ func findPaths(node *TreeNode, path string, paths *[]string) {
 	}
 }
 
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+// Lowest Common Ancestor of a Binary Search Tree
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if p.Val < root.Val && q.Val < root.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	} else if p.Val > root.Val && q.Val > root.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	}
+	return root
+}
+
+// https://leetcode.com/problems/diameter-of-binary-tree/description/
+// Diameter of Binary Tree
+func diameterOfBinaryTree(root *TreeNode) int {
+	_, diameter := depthAndDiameter(root)
+	return diameter
+}
+
+func depthAndDiameter(node *TreeNode) (int, int) {
+	if node == nil {
+		return 0, 0
+	}
+	leftDepth, leftDiameter := depthAndDiameter(node.Left)
+	rightDepth, rightDiameter := depthAndDiameter(node.Right)
+	maxDiameter := max(leftDiameter, rightDiameter)
+	return max(leftDepth, rightDepth) + 1, max(maxDiameter, leftDepth+rightDepth)
+}
+
+// Merge Two Binary Trees
+func mergeTrees(t1, t2 *TreeNode) *TreeNode {
+	if t1 == nil {
+		return t2
+	}
+	if t2 == nil {
+		return t1
+	}
+	t1.Val += t2.Val
+	t1.Left = mergeTrees(t1.Left, t2.Left)
+	t1.Right = mergeTrees(t1.Right, t2.Right)
+	return t1
+}
+
+// Minimum Depth of Binary Tree
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil {
+		return minDepth(root.Right) + 1
+	}
+	if root.Right == nil {
+		return minDepth(root.Left) + 1
+	}
+	return min(minDepth(root.Left), minDepth(root.Right)) + 1
+}
+
+// Range Sum of BST
+func rangeSumBST(root *TreeNode, L int, R int) int {
+	if root == nil {
+		return 0
+	}
+	sum := 0
+	if root.Val >= L && root.Val <= R {
+		sum += root.Val
+	}
+	if root.Val > L {
+		sum += rangeSumBST(root.Left, L, R)
+	}
+	if root.Val < R {
+		sum += rangeSumBST(root.Right, L, R)
+	}
+	return sum
+}
+
+// Utility functions for min and max
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 // Main function to test each problem
 func main() {
 	// Build a sample tree
@@ -134,14 +226,33 @@ func main() {
 	// Test Invert Binary Tree
 	fmt.Println("Inverted Binary Tree (Preorder Traversal):")
 	invertTree(root)
-	printPreorder(root) // Helper function to print the tree in preorder
+	printPreorder(root)
 	fmt.Println()
 
 	// Test Path Sum
-	fmt.Println("Has Path Sum (target 7):", hasPathSum(root, 7))
+	fmt.Println("Has Path Sum (target 10):", hasPathSum(root, 10))
 
 	// Test Binary Tree Paths
 	fmt.Println("Binary Tree Paths:", binaryTreePaths(root))
+
+	// Test Lowest Common Ancestor of a BST
+	p, q := &TreeNode{Val: 2}, &TreeNode{Val: 4}
+	fmt.Println("Lowest Common Ancestor of 2 and 4:", lowestCommonAncestor(root, p, q).Val)
+
+	// Test Diameter of Binary Tree
+	fmt.Println("Diameter of Binary Tree:", diameterOfBinaryTree(root))
+
+	// Test Merge Two Binary Trees
+	mergedTree := mergeTrees(&TreeNode{Val: 1, Left: &TreeNode{Val: 3}}, &TreeNode{Val: 2, Right: &TreeNode{Val: 4}})
+	fmt.Println("Merged Tree (Preorder Traversal):")
+	printPreorder(mergedTree)
+	fmt.Println()
+
+	// Test Minimum Depth of Binary Tree
+	fmt.Println("Minimum Depth of Binary Tree:", minDepth(root))
+
+	// Test Range Sum of BST
+	fmt.Println("Range Sum of BST (Range 4 to 9):", rangeSumBST(root, 4, 9))
 }
 
 // Helper function to print the tree in preorder for visual confirmation
